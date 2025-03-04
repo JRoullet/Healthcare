0167c7df000ce59e1d55ef5eaf1f15c744e39d8a
Jacques <jacques.roullet@laposte.net>
2025-03-04 09:59:56 +0100

    Update - MS WEBAPP + configserver configurations + docker-compose file

---
+++ ms-webapp/src/test/java/jroullet/ms_webapp/MsWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package jroullet.ms_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
